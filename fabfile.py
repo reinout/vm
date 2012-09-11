2089ae079e02c9f083674d171d3146355f84012f
Reinout van Rees <reinout@vanrees.org>
2012-09-11 16:24:49 +0200

    Version-less python-dev

--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,7 @@ from fabric.operations import sudo
 @task
 @hosts('vagrant@33.33.33.20')
 def initial_setup():
-    sudo("apt-get install git python2.7-dev python-virtualenv")
+    sudo("apt-get install git python-dev python-virtualenv")
     if not exists("tools"):
         run("git clone git@github.com:reinout/tools.git")
     if not exists("/Users"):
