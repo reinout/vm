8586beefc58bf57dda0c77cf724be0b060220e7d
Reinout van Rees <reinout@vanrees.org>
2012-09-12 10:35:49 +0200

    Added more dependencies

--- fabfile.py
+++ fabfile.py
@@ -8,19 +8,34 @@ from fabric.operations import sudo
 PACKAGES = [
     'binutils',
     'build-essential',
+    'git',
+    'libjpeg-dev',
     'lynx-cur',
+    'memcached',
+    'nginx',
     'python-dev',
+    'python-gdal',
+    'python-imaging',
+    'python-lxml',
+    # 'python-mapnik',
+    'python-matplotlib',
+    'python-psycopg2',
+    'python-pyproj',
+    'python-pysqlite2',
+    'python-scipy',
+    'python-setuptools',
     'python-virtualenv',
+    'spatialite-bin',
     'subversion',
     'unzip',
-    'git',
-    # TODO pil, mapnik etc
+    # TODO: check some of 'em for non-X-using packages. Too much is grabbed.
     ]
 
 
 @task
 @hosts('vagrant@33.33.33.20')
 def initial_setup():
+    sudo("apt-get update")
     sudo("apt-get install " + ' '.join(PACKAGES))
     if not exists("tools"):
         run("git clone git@github.com:reinout/tools.git")
