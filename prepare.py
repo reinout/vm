0bfb82a29ff85b6c8dfb4b5b75b97ce22ccdcf17
Reinout van Rees <reinout@vanrees.org>
2014-11-26 16:49:08 +0100

    vagrantfile => Vagrantfile

--- prepare.py
+++ prepare.py
@@ -8,6 +8,11 @@ ANSIBLE_CFG_FILENAME = 'ansible.cfg'
 def main():
     vagrantfiles = glob.glob('vagrantfile_*')
     vm_names = [vagrantfile.split('_')[-1] for vagrantfile in vagrantfiles]
+    ignore_contents = [line.strip() for line in open('.gitignore')]
+    hostsfile_contents = [line.strip() for line in open('/etc/hosts')
+                          if line.strip()]
+    known_hostnames = [line.split()[-1] for line in hostsfile_contents]
+
     for vm_name in vm_names:
         if not os.path.exists(vm_name):
             os.mkdir(vm_name)
@@ -17,6 +22,8 @@ def main():
         for relevant_filename in relevant_filenames:
             filename_inside_vm = relevant_filename.replace('_%s' % vm_name,
                                                            '')
+            if filename_inside_vm == 'vagrantfile':
+                filename_inside_vm = 'Vagrantfile'  # Just making sure.
             if not os.path.exists(filename_inside_vm):
                 src = '../%s' % relevant_filename
                 os.symlink(src, filename_inside_vm)
@@ -31,6 +38,17 @@ def main():
                 src = '/Users/reinout/utils'
                 os.symlink(src, 'utils')
                 print("Linked %s to %s" % (src, 'utils'))
+
+        #   config.vm.network "private_network", ip: "10.0.0.12"
+        if vm_name not in ignore_contents:
+            print("%s should be added to .gitignore" % vm_name)
+        if vm_name not in known_hostnames:
+            print("%s should be added to /etc/hosts" % vm_name)
+            print("private_network lines from the vagrant file:")
+            for line in open('Vagrantfile'):
+                if 'private_network' in line:
+                    print(line)
+            print('')
 
         os.chdir('..')
 
