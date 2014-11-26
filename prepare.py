b19bc5c1c76852913bf0cb31029bfc330fef6dd5
Reinout van Rees <reinout@vanrees.org>
2014-11-26 14:13:49 +0100

    pep8

--- prepare.py
+++ prepare.py
@@ -4,6 +4,7 @@ import os
 
 ANSIBLE_CFG_FILENAME = 'ansible.cfg'
 
+
 def main():
     vagrantfiles = glob.glob('vagrantfile_*')
     vm_names = [vagrantfile.split('_')[-1] for vagrantfile in vagrantfiles]
@@ -17,17 +18,17 @@ def main():
             filename_inside_vm = relevant_filename.replace('_%s' % vm_name,
                                                            '')
             if not os.path.exists(filename_inside_vm):
-                src =  '../%s' % relevant_filename
+                src = '../%s' % relevant_filename
                 os.symlink(src, filename_inside_vm)
                 print("Linked %s to %s" % (src, filename_inside_vm))
 
             if not os.path.exists(ANSIBLE_CFG_FILENAME):
-                src =  '../%s' % ANSIBLE_CFG_FILENAME
+                src = '../%s' % ANSIBLE_CFG_FILENAME
                 os.symlink(src, ANSIBLE_CFG_FILENAME)
                 print("Linked %s to %s" % (src, ANSIBLE_CFG_FILENAME))
 
             if not os.path.exists('utils'):
-                src =  '/Users/reinout/utils'
+                src = '/Users/reinout/utils'
                 os.symlink(src, 'utils')
                 print("Linked %s to %s" % (src, 'utils'))
 
